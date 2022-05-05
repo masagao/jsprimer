1fedf9a6ef2cdbba91240a600a9d48c9cc180818
masagao <masanao@gaogao.asia>
2022-05-05 23:41:38 +0900

    add admin app

---
+++ jake_js/admin-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+import { AdminFlagContext } from "./components/providers/AdminFlagProvider";
+import { Card } from "./components/Card";
+
+export const App = () => {
+
+  const { isAdmin, setIsAdmin } = useContext(AdminFlagContext);
+
+  const onClickSwitch = () => setIsAdmin(!isAdmin);
+
+  return (
+    <div>
+      {isAdmin ? <span>管理者です</span> : <span>管理者意外です</span>}
+      <button onClick={onClickSwitch}>切り替え</button>
+      <Card isAdmin={isAdmin} />
+    </div>
+  );
+};
